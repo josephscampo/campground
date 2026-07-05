ae82b6e07c819a87088856013ff8d02f2a117bf7
josephscampo <josephscampo@gmail.com>
2026-07-05 11:14:27 -0400

    fixed the bootloader

--- nixos-config/platform/bootloader.nix
+++ nixos-config/platform/bootloader.nix
@@ -4,15 +4,32 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # 1. Hardware network modules for the NUC
-  boot.initrd.availableKernelModules = [ "r8169" "e1000e" "igb" ];
+  # 1. Hardware network modules for early boot networking
+  boot.initrd.availableKernelModules = [ "r8169" "e1000e" "igb" "igc" ];
+  boot.initrd.kernelModules = [ "r8169" "e1000e" "igb" "igc" ];
+
+  # Make the initramfs SSH host key available during early boot so remote unlock can work.
+  boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.network.enable = true;
+  boot.initrd.systemd.network.networks."10-dhcp" = {
+    matchConfig = {
+      Name = "en*";
+    };
+    networkConfig = {
+      DHCP = "yes";
+      IPv6AcceptRA = "yes";
+    };
+  };
+  boot.initrd.secrets = {
+    "/etc/secrets/initrd_ssh_host_ed25519_key" = "/etc/secrets/initrd_ssh_host_ed25519_key";
+  };
 
   # 2. Key-Based Boot Stage SSH
   boot.initrd.network = {
     enable = true;
     ssh = {
       enable = true;
-      port = 22;
+      port = 422;
       
       # Paste your laptop's public key here to allow it to bypass passwords on boot
       authorizedKeys = [ 
@@ -23,4 +40,7 @@
       hostKeys = [ "/etc/secrets/initrd_ssh_host_ed25519_key" ];
     };
   };
+
+  # Force asking for password
+  boot.initrd.systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
 }
